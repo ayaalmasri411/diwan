44b917bb7c68ca7278a422e6f84f45f936ce4bba
ayaalmasri411 <ayaalmasri411@gmail.com>
2026-05-04 01:23:13 -0700

    Delete src directory

--- src/services/api.ts
+++
@@ -1,9 +0,0 @@
-export async function login(username: string, password: string) {
-  const response = await fetch("http://localhost:3001/api/login", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ username, password }),
-  });
-
-  return response.json();
-}

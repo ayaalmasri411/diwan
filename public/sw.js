80f3b5f9ede85217a7a6446b6bdb2f576d2383fa
ayaalmasri411 <ayaalmasri411@gmail.com>
2026-05-04 01:24:30 -0700

    Delete public directory

--- public/sw.js
+++
@@ -1,8 +0,0 @@
-self.addEventListener('install', (event) => {
-  self.skipWaiting();
-});
-
-self.addEventListener('fetch', (event) => {
-  // basic bypass to allow PWA criteria
-  event.respondWith(fetch(event.request));
-});
